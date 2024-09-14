69a112ef5d1c1cd4a2b0c80ca779bd003c5d79f8
AtharvArolkar <arolkaratharv2000@gmail.com>
2024-09-14 19:16:21 +0530

    Fetch 1

--- src/helpers/api-service.ts
+++ src/helpers/api-service.ts
@@ -42,3 +42,46 @@ export const callApi = async <Type>(
       return await axios.put(url, payload, callOption);
   }
 };
+
+export const callApi1 = async <Type>(
+  url: string,
+  method: ApiMethod,
+  token?: string,
+  payload?:
+    | GetAccessRefreshPayload
+    | ChangePasswordPayload
+    | GetUserPayload
+    | GetTicketsPayload
+    | AddTicketPayload
+    | UserListPayload
+    | AddUserPayload
+    | string
+): Promise<Type> => {
+  const options: RequestInit = {
+    method: method.toString(),
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token || ""}`,
+    },
+    body:
+      payload && method !== ApiMethod.GET ? JSON.stringify(payload) : undefined,
+  };
+
+  try {
+    const response = await fetch(url, options);
+
+    if (!response.ok) {
+      const errorText = await response.text();
+      console.log(errorText);
+      // throw new Error(
+      //   `HTTP error! status: ${response.status}, message: ${errorText}`
+      // );
+    }
+
+    const data: Type = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Fetch error:", error);
+    throw error;
+  }
+};
